abcba6b712f95a3dcdd0bd1b3d6c65c175b163f6
mosesjohn6969 <mosesjohn12345>
2021-08-25 20:41:53 +0100

    Finall Tutorial

--- market/__init__.py
+++ market/__init__.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = 'd843990ef0dfa55e935a4e75'
 db = SQLAlchemy(app)
-
-from market  import routes
-
-
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,9 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, request
 from market.model import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 @app.route('/')
@@ -11,15 +12,38 @@ def home_page():
     return render_template('home.html')
 
 
-@app.route('/market')
+@app.route('/market', methods=['GET', 'POST'])
+@login_required
 def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
+    purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
+    if request.method == 'POST':
+        # Purchase Item Logic
+        purchase_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchase_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f'Congratulations! you Purchased {p_item_object.name} for ₦{p_item_object.price}',
+                      category="success")
+            else:
+                flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}", category="danger")
+        # Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f'Congratulations! you Sold {s_item_object.name} Back to market for ₦{s_item_object.price}', category="success")
+            else:
+                flash(f"Unfortunately, Something Went Wrong with Sell {s_item_object.name}", category="danger")
 
+        return redirect(url_for('market_page'))
 
-@app.route('/login')
-def login_page():
-    return render_template('login.html')
+    if request.method == "GET":
+        items = Item.query.filter_by(owner=None)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items, selling_form=selling_form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -28,11 +52,34 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash=form.password1.data)
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account Created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
-    if form.errors != {}: # if No Errors from Validation
+    if form.errors != {}:  # if No Errors from Validation
         for err_msg in form.errors.values():
-            print(f'there was an Error Creating a user{err_msg}')
+            flash(f'there was an Error Creating a user{err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+            login_user(attempted_user)
+            flash(f'Success! You logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You've Been Loggged out!", category='info')
+    return redirect(url_for('home_page'))
